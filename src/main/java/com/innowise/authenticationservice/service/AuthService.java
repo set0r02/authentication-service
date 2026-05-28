bb14764c910e903039acb3de1bb78b1444673bc0
setor <marik123456789110011@gmail.com>
2026-05-28 22:31:00 +0300

    feet: fix AuthService interface

--- src/main/java/com/innowise/authenticationservice/service/AuthService.java
+++ src/main/java/com/innowise/authenticationservice/service/AuthService.java
@@ -3,10 +3,15 @@ package com.innowise.authenticationservice.service;
 
 import com.innowise.authenticationservice.dto.request.AuthRequest;
 import com.innowise.authenticationservice.dto.response.TokenResponseDto;
+import com.innowise.authenticationservice.dto.response.TokenValidResponseDto;
 
 public interface AuthService {
 
     Long register(AuthRequest authRequest);
 
     TokenResponseDto login(AuthRequest authRequest);
+
+    TokenValidResponseDto validate(String token);
+
+    TokenResponseDto refresh(String token);
 }
