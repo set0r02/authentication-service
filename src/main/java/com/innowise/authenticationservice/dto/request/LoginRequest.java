a9060352b77d6d9a1c30edf719f8b25c2dcada73
setor <marik123456789110011@gmail.com>
2026-05-29 19:37:57 +0300

    validation for request dto's

--- src/main/java/com/innowise/authenticationservice/dto/request/LoginRequest.java
+++ src/main/java/com/innowise/authenticationservice/dto/request/LoginRequest.java
@@ -1,6 +1,16 @@
 package com.innowise.authenticationservice.dto.request;
 
-public record LoginRequest(String login,
-                           String password
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequest(
+
+        @NotBlank(message = "Login should not be blank")
+        @Size(min = 3, max = 30, message = "Login should be between {min} and {max} characters")
+        String login,
+
+        @NotBlank(message = "Password should not be blank")
+        @Size(min = 8, max = 60, message = "Password should be between {min} and {max} characters")
+        String password
 ) {
 }
